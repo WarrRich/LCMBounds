957733c2b5dbeb71371a2f318f544f70cb2a8e35
WarrRich <richard.l.warr@gmail.com>
2019-06-07 15:10:58 -0600

    changes to the find_support function so that it works

--- R/find_support.R
+++ R/find_support.R
@@ -1,43 +1,37 @@
-#' Sample from the Posterior Distribution of the Linear Gaussian Feature
-#' Allocation Model
+#' Short Title description
 #'
-#' This function samples from the posterior distribution of the linear Gaussian
-#' latent feature model (LGLFM) using an Indian buffet process (IBP) prior on
-#' the feature allocations.
+#' Long Description.
 #'
 #' @param weights A list of numeric vectors that correspond to the multinomial probabilities.
 #' @param samplesizes A numeric vector of the multinomial sample sizes.
 #' @param power.of.2 logical variable that if set to TRUE will return a support whose length is a power of 2.
 #'
-#' @importFrom pracma Lcm
-#' @export
+#' @importFrom pracma gcd
 #' @examples
 #' weights <- list(c(0,1,1),c(2,0,3),c(5,3,0))
 #' samplesizes <- c(20,20,20)
 #' find_support(weights,samplesizes)
 #'
 
+####################################################
+# Function to find an evenly spaced grid for L_hat #
+####################################################
+# Inputs are: the weight matrix -- w
+#             the sample size -- samplesize
 
-#########################################################################
-# Function to find an evenly spaced grid for L_hat                      #
-#########################################################################
-# Inputs are: the list of weights -- "weights"
-#             the vector of each multinomial sample size -- "samplesizes"
-# Returns the support in the form of a vector (rounded to the 14th decimal place)
 find_support <- function(weights,samplesizes,power.of.2=TRUE) {
-  require(pracma)
-  LCM <- 1
-  for (i in 1:length(samplesizes)) {
-    LCM <- Lcm(LCM,samplesizes[i])
+  supp <- possible.support(weights,samplesizes)
+  stepsizes <- round(supp[-1]-supp[-length(supp)],14)
+  smalleststepsize <- stepsizes[1]
+  for (i in 1:length(stepsizes)) {
+    smalleststepsize <- gcd(smalleststepsize,stepsizes[i])
   }
-  max.weights <- sum(unlist(lapply(weights,max)))
-  min.weights <- sum(unlist(lapply(weights,min)))
-  support <- round(seq(from=min.weights, to=max.weights, by=1/LCM),14)
+  supp <- round(seq(from=min(supp),to=max(supp),by=smalleststepsize),14)
   if (power.of.2) {
     # making the length of the support a power of 2
-    len <- length(support)
+    len <- length(supp)
     extras <- 2^(as.integer(log(len-0.1)/log(2))+1)
-    round(seq(from=min.weights, by=1/LCM, length.out=extras),14)
+    supp <- round(seq(from=min(supp), by=smalleststepsize, length.out=extras),14)
   }
-  support
+  supp
 }
---
+++ R/possiblesupport.R
@@ -0,0 +1,41 @@
+#' Short Title description
+#'
+#' Long Description.
+#'
+#' @param weights A list of numeric vectors that correspond to the multinomial probabilities.
+#' @param samplesizes A numeric vector of the multinomial sample sizes.
+#'
+#' @export
+#' @examples
+#' weights <- list(c(0,1,1),c(2,0,3),c(5,3,0))
+#' samplesizes <- c(20,20,20)
+#' possible.support(weights,samplesizes)
+#'
+
+####################################################
+# Function to find an evenly spaced grid for L_hat #
+####################################################
+# Inputs are: the weight matrix -- w
+#             the sample size -- samplesize
+
+possible.support <- function(weights,samplesizes) {
+  tempsupp <- vector("list",length=length(weights))
+  for (i in 1:length(weights)) {
+    if (samplesizes[i] > 1) {
+      tempsupp[[i]] <- unique(weights[[i]])
+      for (j in 2:samplesizes[i]) {
+        tempsupp[[i]] <- unique(as.vector(outer(tempsupp[[i]],weights[[i]],FUN=function(x,y) x+y)))
+      }
+    } else {
+      tempsupp[[i]] <- unique(weights[[i]])
+    }
+    tempsupp[[i]] <- sort(tempsupp[[i]])/samplesizes[i]
+  }
+  supp <- tempsupp[[1]]
+  if (length(weights) > 1) {
+    for (i in 2:length(weights)) {
+      supp <- unique(as.vector(outer(supp,tempsupp[[i]],FUN=function(x,y) x+y)))
+    }
+  }
+  sort(unique(round(supp,15)))
+}
