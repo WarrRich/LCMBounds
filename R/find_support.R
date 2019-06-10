8c8d443fbc280cefa4abcb54a08acf7635d281a7
WarrRich <richard.l.warr@gmail.com>
2019-06-10 14:26:16 -0600

    changes to make things work

--- R/find_support.R
+++ R/find_support.R
@@ -6,7 +6,7 @@
 #' @param samplesizes A numeric vector of the multinomial sample sizes.
 #' @param power.of.2 logical variable that if set to TRUE will return a support whose length is a power of 2.
 #'
-#' @importFrom pracma gcd
+#' @importFrom pracma Lcm
 #' @examples
 #' weights <- list(c(0,1,1),c(2,0,3),c(5,3,0))
 #' samplesizes <- c(20,20,20)
@@ -20,18 +20,18 @@
 #             the sample size -- samplesize
 
 find_support <- function(weights,samplesizes,power.of.2=TRUE) {
-  supp <- possible.support(weights,samplesizes)
-  stepsizes <- round(supp[-1]-supp[-length(supp)],14)
-  smalleststepsize <- stepsizes[1]
-  for (i in 1:length(stepsizes)) {
-    smalleststepsize <- gcd(smalleststepsize,stepsizes[i])
+  lcm <- 1
+  for (i in 1:length(samplesizes)) {
+    lcm <- Lcm(lcm,samplesizes[i])
   }
-  supp <- round(seq(from=min(supp),to=max(supp),by=smalleststepsize),14)
+  minval <- sum(unlist(lapply(weightsD,min)))
+  maxval <- sum(unlist(lapply(weightsD,max)))
+  support <- seq(from=minval,to=maxval,by = 1/lcm)
   if (power.of.2) {
     # making the length of the support a power of 2
-    len <- length(supp)
+    len <- length(support)
     extras <- 2^(as.integer(log(len-0.1)/log(2))+1)
-    supp <- round(seq(from=min(supp), by=smalleststepsize, length.out=extras),14)
+    support <- seq(from=minval, by=1/lcm, length.out=extras)
   }
-  supp
+  round(support,14)
 }
