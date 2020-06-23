402d1b6f56badb9bbeed6579e0284149fdee7a5d
WarrRich <richard.l.warr@gmail.com>
2020-06-23 12:10:49 -0600

    Updating documentation

--- R/LCMBoundsMean.R
+++ R/LCMBoundsMean.R
@@ -1,37 +1,27 @@
-#' Finds a Confindence Interval for the Linear Combination of Multinomial Probabilities
+#' Finds an average coverage confindence interval for a linear combination of multinomial probabilities
 #'
-#' @param Lhat Observed value that needs a Confidence Interval (a scalar).
+#' @param Lhat Observed value of a linear combination of multinomial probabilities that needs a confidence interval (a scalar).
 #' @param weights A list of numeric vectors that correspond to the multinomial probabilities.
-#' @param samplesizes A numeric vector of the sample sizes for the K multinomial experiements.
-#' @param alpha.upper Not sure Yet.
-#' @param alpha.lower Not sure Yet.
-#' @param rand.samps Not sure Yet.
-#' @param opt.samps Not sure Yet.
-#' @param mass Not sure Yet.
+#' @param samplesizes A numeric vector of length K, each entry in the vector cooresponds to the sample sizes for the K multinomial experiements.
+#' @param alpha.upper The amount of probability outside the upper limit of the confidence interval (this produces a (1-alpha.upper-alpha.lower)% CI).
+#' @param alpha.lower The amount of probability outside the lower limit of the confidence interval.
+#' @param rand.samps A scalar which directs the alogrithm on how many random samples to try before beginning the optimizing stage.
+#' @param opt.samps A scalar which directs the alogrithm on how many optimizing steps to make.
+#' @param mass A scalar tuning parameter.  It is not recommended to change it from the default.
+#' @param power.of.2 A logical tuning parameter.  It is not recommended to change it from the default.
 #'
 #' @importFrom stats uniroot
 #' @export
 #' @examples
 #' Lhat <- 7.8
 #' weightsA <- list(c(0,1,1),c(2,0,3),c(5,3,0))
-#' samplesizes5 <- c(20,20,20)
-#' LCMBoundsMean(Lhat,weightsA,samplesizes5,rand.samps=200,opt.samps=1,mass=1)
+#' samplesizes <- c(20,20,20)
+#' LCMBoundsMean(Lhat,weightsA,samplesizes,opt.samps=3)
 #'
 
 LCMBoundsMean <- function(Lhat,weights,samplesizes,alpha.upper=0.025,alpha.lower=alpha.upper,
-                        rand.samps=200,opt.samps=200,mass=1,power.of.2=TRUE) {
-  ####################################################################
-  # Lhat is the observed statisitcs a scalar
-  # weights is a list m vectors, each vector is the weights for that multinomial experiment
-  # samplesizes is a vector of length m which gives the sample size for each multinomial experiment
-  # alpha.upper
-  # alpha.lower
-  # rand.samps is a scalar which tell the alogrithm how many random samples to try before optimizing
-  # opt.samps is a scalar which tell the alogrithm how many iterations to take using stochastic optimization
-  # search.num is the number of iterations to search for bound on Lhat
-  # mass is a scalar
-  ####################################################################
-  
+                        rand.samps=20,opt.samps=20,mass=1,power.of.2=TRUE) {
+
   ##########################################
   # Tests to ensure the inputs are valid
   
